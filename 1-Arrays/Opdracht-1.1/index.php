5b8638d929787c0acd14935c18f6184b9de5beb7
EricRocVa <2105269@talnet.nl>
2022-02-14 10:43:42 +0100

    advanced php 1.1 af

---
+++ 1-Arrays/Opdracht-1.1/index.php
@@ -0,0 +1,8 @@
+<?php
+$student['voornaam'] = "Pharrell";
+$student['achternaam'] = "hellstone";
+$student['leeftijd'] = 16;
+$student['klas'] = '1D';
+
+echo "Gegevens Pharell: Voornaam:". " " . " " . $student['voornaam'] . " " . "Achternaam:" . $student['achternaam'] . " " . "Leeftijd:" . " " . $student['leeftijd'] . " ". "Klas:" . " " . $student['klas'];
+?> 
