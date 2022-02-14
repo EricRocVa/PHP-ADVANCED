d7e4bb66a7aeb120893daf516f4c8c718dd23897
EricRocVa <2105269@talnet.nl>
2022-02-14 10:45:21 +0100

    advanced PHP opdracht 1.2 af

---
+++ 1-Arrays/Opdracht-1.2/index.php
@@ -0,0 +1,7 @@
+<?php
+$mezelf['voornaam'] = "Eric";
+$mezelf['achternaam'] = "Oehlers";
+$mezelf['leeftijd'] = 20;
+$mezelf['opleiding'] = "Software Developer.";
+
+echo "Mijn naam is" . " " . $mezelf['voornaam'] . " " . $mezelf['achternaam'] . " " . ", ik ben" . " " .  $mezelf['leeftijd'] . " " . "jaar oud". " " . "Ik zit op het ROC van amsterdam en ik doe de opleiding" . " " . $mezelf['opleiding']; 
