934e0cb5b45b4edf22034ec6ed3a56c66a9c839a
EricRocVa <2105269@talnet.nl>
2022-02-15 15:02:57 +0100

    opdracht 3.1 af

---
+++ 3-Forms/Opdracht-3.1/mijn invoer.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <form method="post" action="mijngegevens.php">
+    <form method="GET" action="mijngegevens.php">
+        <label>Voornaam:</label>
+        <input type="text" name= "voornaam">
+        <input type="text" name="voornaam">
+        <label>Achternaam:</label>
+        <input type="text" name= "achternaam">
+        <input type="text" name="achternaam">
+        <label>klas:</label>
+        <input type="klas"  name= "klas">
+        <input type="klas"  name="klas">
+        <label>leeftijd:</label>
+        <input type="text" name= "leeftijd">
+        <input type="text" name="leeftijd">
+        <label>Adres:</label>
+        <input type="text" name= "adres">
+        <input type="text" name="adres">
+        <label>Plaatsnaam:</label>
+        <input type="text" name= "plaatsnaam">
+        <input type="text" name="plaatsnaam">
+    <input type="submit" name="submit"value="verstuur">
+    </form>   
+</body>
+</html>
